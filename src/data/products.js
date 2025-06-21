e160f52a959c4d2b3841c5631063122c166340af
Petermwansa <mwansapeter800@gmail.com>
2025-06-22 00:55:05 +0300

    Add the logic to the Component file to display and filter the products

---
+++ src/data/products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 1,
+    photo: 'https://via.placeholder.com/80',
+    price: 29.99,
+    color: 'Red',
+    size: 'M',
+  },
+  {
+    id: 2,
+    photo: 'https://via.placeholder.com/80',
+    price: 19.99,
+    color: 'Blue',
+    size: 'L',
+  },
+  {
+    id: 3,
+    photo: 'https://via.placeholder.com/80',
+    price: 39.99,
+    color: 'Green',
+    size: 'S',
+  },
+  {
+    id: 4,
+    photo: 'https://via.placeholder.com/80',
+    price: 25.5,
+    color: 'Red',
+    size: 'L',
+  },
+];
+
+export default products;
